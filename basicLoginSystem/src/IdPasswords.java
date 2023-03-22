f731ad7ad34deb4285433d618fc70c40cce49a3a
Colin <choude4@gmail.com>
2023-03-22 16:10:59 -0400

    added login algorithm with create user functino
    also added SHA 256 encryption of the password string

--- basicLoginSystem/src/IdPasswords.java
+++ basicLoginSystem/src/IdPasswords.java
@@ -1,15 +1,41 @@
+import java.security.MessageDigest;
 import java.util.HashMap;
 public class IdPasswords {
     // hashmap to store key value pairs of username and passwords
     HashMap<String, String> loginSheet = new HashMap<>();
     IdPasswords(){
-        loginSheet.put("admin", "admin");
+        loginSheet.put("admin", sha256("admin"));
     }
     IdPasswords(String x, String y){
         loginSheet.put(x, y);
     }
     protected HashMap<String, String> getLoginInfo(){
         return loginSheet;
+    }
+
+    public static String sha256(final String base) {
+        try{
+            final MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            final byte[] hash = digest.digest(base.getBytes("UTF-8"));
+            final StringBuilder hexString = new StringBuilder();
+            for (int i = 0; i < hash.length; i++) {
+                final String hex = Integer.toHexString(0xff & hash[i]);
+                if(hex.length() == 1)
+                    hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } catch(Exception ex){
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public static void main(String[] args) {
+        // testing the hash function
+//        String whatever = sha256("hi");
+//        String whatever2 = sha256("hi");
+//        System.out.println(whatever);
+//        System.out.println(whatever2);
     }
 
 }
--- basicLoginSystem/src/LoginPage.java
+++ basicLoginSystem/src/LoginPage.java
@@ -11,6 +11,7 @@ public class LoginPage implements ActionListener {
     JFrame frame = new JFrame();
     JButton loginButton = new JButton("Login");
     JButton reset = new JButton("Reset");
+    JButton createUser = new JButton("Create User");
 
     // need a user Id field to input text
     JTextField idField = new JTextField();
@@ -28,7 +29,7 @@ public class LoginPage implements ActionListener {
         idLabel.setBounds(50,100,75,25);
         passLabel.setBounds(50,150,75,25);
 
-        loginResult.setBounds(125,250,250,35);
+        loginResult.setBounds(162,300,250,35);
         loginResult.setFont(new Font(null, Font.ITALIC, 25));
 
         idField.setBounds(125,100,200,25);
@@ -42,6 +43,10 @@ public class LoginPage implements ActionListener {
         reset.setFocusable(false);
         reset.addActionListener(this);
 
+        createUser.setBounds(162,250,125,25);
+        createUser.setFocusable(false);
+        createUser.addActionListener(this);
+
         frame.add(idLabel);
         frame.add(passLabel);
         frame.add(loginResult);
@@ -49,6 +54,7 @@ public class LoginPage implements ActionListener {
         frame.add(passField);
         frame.add(loginButton);
         frame.add(reset);
+        frame.add(createUser);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(420,420);
         frame.setLayout(null);
@@ -57,15 +63,37 @@ public class LoginPage implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == createUser){
+            if(loginInfo.containsKey(idField.getText())){
+                // the username is already taken
+                loginResult.setForeground(Color.yellow);
+                loginResult.setText("Username already taken");
+            }
+            else {
+                // add the new user
+                String userId = idField.getText();
+                String userPass = String.valueOf(passField.getPassword());
+                // encrypt the password
+                userPass = IdPasswords.sha256(userPass);
+                loginInfo.put(userId, userPass);
+                loginResult.setForeground(Color.green);
+                loginResult.setText("User Created!");
+            }
+        }
+
+
         if(e.getSource()== reset){
             idField.setText("");
             passField.setText("");
+            loginResult.setText("");
         }
 
         if(e.getSource() == loginButton){
             // first check if the userId is within the HashMap
             String userId = idField.getText();
             String userPass = String.valueOf(passField.getPassword());
+            // encrypt it
+            userPass = IdPasswords.sha256(userPass);
             if(loginInfo.containsKey(userId)){
                 if(loginInfo.get(userId).equals(userPass)){
                     loginResult.setForeground(Color.green);
@@ -74,12 +102,12 @@ public class LoginPage implements ActionListener {
                 }
                 else {
                     loginResult.setForeground(Color.red);
-                    loginResult.setText("Login Failed");
+                    loginResult.setText("Incorrect Password");
                 }
             }
             else {
                 loginResult.setForeground(Color.red);
-                loginResult.setText("Login Failed");
+                loginResult.setText("User not found");
             }
 
             // if it is correct return the index of it for later saving
